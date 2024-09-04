d954ccb5be2df4a91124c8e19abb31605e7a069a
国wei <ygweric@gmail.com>
2024-09-04 09:59:20 +0800

    34

--- scripts/syncImagesToPublic.mjs
+++ scripts/syncImagesToPublic.mjs
@@ -1,18 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import path from 'path'
 // eslint-disable-next-line import/no-rename-default
 import syncDirectory from 'sync-directory'
 
-
-const folders = [
-  'cesium',
-  'tools',
-]
 export const syncImagesToPublic = (watch = true) => {
-
-  folders.forEach(folder => {
-    syncDirectory(`./docs/${folder}`, `./docs/.vuepress/public/images/docs/${folder}`, {
-      watch,
-      exclude: [/.*(md|mdx)$/i],
-    })
+  syncDirectory(`./docs/`, `./docs/.vuepress/public/images/docs/`, {
+    watch,
+    exclude: [
+      /.*(md|mdx)$/i,
+      /^\..*$/i, // '.obsidian', '.vuepress', etc
+    ],
   })
-
 }
