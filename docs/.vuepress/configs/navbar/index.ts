407993693f6e9bc2ad2677c6f17bc816dc1568ee
国wei <ygweric@gmail.com>
2024-09-04 12:24:16 +0800

    Blog updated by Obsidian at 2024-09-04 12:24:16

--- docs/.vuepress/configs/navbar/index.ts
+++ docs/.vuepress/configs/navbar/index.ts
@@ -1,144 +1,146 @@
 import type { NavbarOptions } from '@vuepress/theme-default'
 // import { VERSION } from '../meta.js'
+import nav from '../../../config/nav.json'
 
-export const navbarZh: NavbarOptions = [
-  {
-    text: 'Cesium',
-    link: '/cesium/',
-  },
-  {
-    text: '前端',
-    link: '/fe/',
-  },
-  {
-    text: '开发工具',
-    link: '/tools/',
-  },
-  // {
-  //   text: '指南',
-  //   children: [
-  //     '/guide/introduction.md',
-  //     '/guide/getting-started.md',
-  //     '/guide/configuration.md',
-  //     '/guide/page.md',
-  //     '/guide/markdown.md',
-  //     '/guide/assets.md',
-  //     '/guide/i18n.md',
-  //     '/guide/deployment.md',
-  //     '/guide/theme.md',
-  //     '/guide/plugin.md',
-  //     '/guide/bundler.md',
-  //     '/guide/migration.md',
-  //     '/guide/troubleshooting.md',
-  //   ],
-  // },
-  // {
-  //   text: '指南',
-  //   children: [
-  //     '/guide/introduction.md',
-  //     '/guide/getting-started.md',
-  //     '/guide/configuration.md',
-  //     '/guide/page.md',
-  //     '/guide/markdown.md',
-  //     '/guide/assets.md',
-  //     '/guide/i18n.md',
-  //     '/guide/deployment.md',
-  //     '/guide/theme.md',
-  //     '/guide/plugin.md',
-  //     '/guide/bundler.md',
-  //     '/guide/migration.md',
-  //     '/guide/troubleshooting.md',
-  //   ],
-  // },
-  // {
-  //   text: '参考',
-  //   children: [
-  //     {
-  //       text: '核心',
-  //       children: [
-  //         '/reference/cli.md',
-  //         '/reference/yang-test.md',
-  //         '/reference/config.md',
-  //         '/reference/frontmatter.md',
-  //         '/reference/components.md',
-  //         '/reference/plugin-api.md',
-  //         '/reference/theme-api.md',
-  //         '/reference/client-api.md',
-  //         '/reference/node-api.md',
-  //       ],
-  //     },
-  //     {
-  //       text: '打包工具',
-  //       children: [
-  //         '/reference/bundler/vite.md',
-  //         '/reference/bundler/webpack.md',
-  //       ],
-  //     },
-  //     {
-  //       text: '生态系统',
-  //       children: [
-  //         {
-  //           text: '默认主题',
-  //           link: 'https://ecosystem.vuejs.press/themes/default/',
-  //         },
-  //         {
-  //           text: '插件',
-  //           link: 'https://ecosystem.vuejs.press/plugins/',
-  //         },
-  //       ],
-  //     },
-  //   ],
-  // },
-  // {
-  //   text: '了解更多',
-  //   children: [
-  //     {
-  //       text: '深入',
-  //       children: [
-  //         '/advanced/architecture.md',
-  //         '/advanced/plugin.md',
-  //         '/advanced/theme.md',
-  //         {
-  //           text: 'Cookbook',
-  //           link: '/advanced/cookbook/',
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       text: '其他资源',
-  //       children: [
-  //         {
-  //           text: '生态系统',
-  //           link: 'https://ecosystem.vuejs.press/',
-  //         },
-  //         {
-  //           text: '市场',
-  //           link: 'https://marketplace.vuejs.press/',
-  //         },
-  //         {
-  //           text: '贡献指南',
-  //           link: 'https://github.com/vuepress/core/blob/main/CONTRIBUTING_zh.md',
-  //         },
-  //       ],
-  //     },
-  //   ],
-  // },
-  // {
-  //   text: `v${VERSION}`,
-  //   children: [
-  //     {
-  //       text: '更新日志',
-  //       link: 'https://github.com/vuepress/core/blob/main/CHANGELOG.md',
-  //     },
-  //     {
-  //       text: 'v1.x',
-  //       link: 'https://v1.vuepress.vuejs.org/',
-  //     },
-  //     {
-  //       text: 'v0.x',
-  //       link: 'https://v0.vuepress.vuejs.org/',
-  //     },
-  //   ],
-  // },
-  // TODO: remove the type assertion
-] as NavbarOptions
+export const navbarZh: NavbarOptions = nav as NavbarOptions
+// [
+//   {
+//     text: 'Cesium',
+//     link: '/cesium/',
+//   },
+//   {
+//     text: '前端',
+//     link: '/fe/',
+//   },
+//   {
+//     text: '开发工具',
+//     link: '/tools/',
+//   },
+// {
+//   text: '指南',
+//   children: [
+//     '/guide/introduction.md',
+//     '/guide/getting-started.md',
+//     '/guide/configuration.md',
+//     '/guide/page.md',
+//     '/guide/markdown.md',
+//     '/guide/assets.md',
+//     '/guide/i18n.md',
+//     '/guide/deployment.md',
+//     '/guide/theme.md',
+//     '/guide/plugin.md',
+//     '/guide/bundler.md',
+//     '/guide/migration.md',
+//     '/guide/troubleshooting.md',
+//   ],
+// },
+// {
+//   text: '指南',
+//   children: [
+//     '/guide/introduction.md',
+//     '/guide/getting-started.md',
+//     '/guide/configuration.md',
+//     '/guide/page.md',
+//     '/guide/markdown.md',
+//     '/guide/assets.md',
+//     '/guide/i18n.md',
+//     '/guide/deployment.md',
+//     '/guide/theme.md',
+//     '/guide/plugin.md',
+//     '/guide/bundler.md',
+//     '/guide/migration.md',
+//     '/guide/troubleshooting.md',
+//   ],
+// },
+// {
+//   text: '参考',
+//   children: [
+//     {
+//       text: '核心',
+//       children: [
+//         '/reference/cli.md',
+//         '/reference/yang-test.md',
+//         '/reference/config.md',
+//         '/reference/frontmatter.md',
+//         '/reference/components.md',
+//         '/reference/plugin-api.md',
+//         '/reference/theme-api.md',
+//         '/reference/client-api.md',
+//         '/reference/node-api.md',
+//       ],
+//     },
+//     {
+//       text: '打包工具',
+//       children: [
+//         '/reference/bundler/vite.md',
+//         '/reference/bundler/webpack.md',
+//       ],
+//     },
+//     {
+//       text: '生态系统',
+//       children: [
+//         {
+//           text: '默认主题',
+//           link: 'https://ecosystem.vuejs.press/themes/default/',
+//         },
+//         {
+//           text: '插件',
+//           link: 'https://ecosystem.vuejs.press/plugins/',
+//         },
+//       ],
+//     },
+//   ],
+// },
+// {
+//   text: '了解更多',
+//   children: [
+//     {
+//       text: '深入',
+//       children: [
+//         '/advanced/architecture.md',
+//         '/advanced/plugin.md',
+//         '/advanced/theme.md',
+//         {
+//           text: 'Cookbook',
+//           link: '/advanced/cookbook/',
+//         },
+//       ],
+//     },
+//     {
+//       text: '其他资源',
+//       children: [
+//         {
+//           text: '生态系统',
+//           link: 'https://ecosystem.vuejs.press/',
+//         },
+//         {
+//           text: '市场',
+//           link: 'https://marketplace.vuejs.press/',
+//         },
+//         {
+//           text: '贡献指南',
+//           link: 'https://github.com/vuepress/core/blob/main/CONTRIBUTING_zh.md',
+//         },
+//       ],
+//     },
+//   ],
+// },
+// {
+//   text: `v${VERSION}`,
+//   children: [
+//     {
+//       text: '更新日志',
+//       link: 'https://github.com/vuepress/core/blob/main/CHANGELOG.md',
+//     },
+//     {
+//       text: 'v1.x',
+//       link: 'https://v1.vuepress.vuejs.org/',
+//     },
+//     {
+//       text: 'v0.x',
+//       link: 'https://v0.vuepress.vuejs.org/',
+//     },
+//   ],
+// },
+// TODO: remove the type assertion
+// ] as NavbarOptions
--- docs/.vuepress/configs/sidebar/index.ts
+++ docs/.vuepress/configs/sidebar/index.ts
@@ -1,7 +1,7 @@
 import type { SidebarOptions } from '@vuepress/theme-default'
-import cesium from './cesium.json'
-import fe from './fe.json'
-import tools from './tools.json'
+import cesium from '../../../config/cesium.json'
+import fe from '../../../config/fe.json'
+import tools from '../../../config/tools.json'
 
 export const sidebarZh: SidebarOptions = {
   '/cesium/': cesium,
