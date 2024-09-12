19e82e7fdcae0f85f56cdcc1995f9bc73b78d322
国wei <ygweric@gmail.com>
2024-09-12 18:18:27 +0800

    2

--- scripts/commands/sync-files-to-json.mjs
+++ scripts/commands/sync-files-to-json.mjs
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-const */
 /* eslint-disable no-unused-vars */
 /* eslint-disable import/order */
 // /* eslint-disable no-unused-vars */
@@ -7,7 +8,24 @@
 import path from 'path'
 import fs from 'fs'
 
-function getMarkdownFiles(dir, baseDir) {
+function extractMdPaths(data) {
+  const mdPaths = []
+
+  function recursiveSearch(items) {
+    for (let item of items) {
+      if (typeof item === 'string' && item.endsWith('.md')) {
+        mdPaths.push(item) // 如果是 .md 文件路径，添加到数组中
+      } else if (item.children && Array.isArray(item.children)) {
+        recursiveSearch(item.children) // 递归处理子节点
+      }
+    }
+  }
+
+  recursiveSearch(data) // 开始递归
+  return mdPaths
+}
+
+function getMarkdownFiles(dir, baseDir, usedPaths) {
   // eslint-disable-next-line prefer-template
   const relativepath = '/' + path.relative(baseDir, dir).replace(/\\/g, '/')
   const result = {
@@ -25,16 +43,18 @@ function getMarkdownFiles(dir, baseDir) {
 
     if (stat.isDirectory()) {
       // 如果是目录，递归获取子文件夹的 md 文件
-      const children = getMarkdownFiles(fullPath, baseDir)
+      const children = getMarkdownFiles(fullPath, baseDir, usedPaths)
       if (children.children.length > 0) {
         result.children.push(children)
       }
     } else if (stat.isFile() && path.extname(fullPath) === '.md') {
       // 如果是 .md 文件，添加到结果中
-      result.children.push(
-        // eslint-disable-next-line prefer-template
-        '/' + path.relative(baseDir, fullPath).replace(/\\/g, '/'),
-      ) // 转换为相对路径
+      // eslint-disable-next-line prefer-template
+      const mdPath = '/' + path.relative(baseDir, fullPath).replace(/\\/g, '/') // 转换为相对路径
+
+      if (!usedPaths.includes(mdPath)) {
+        result.children.push(mdPath)
+      }
     }
   })
 
@@ -50,14 +70,24 @@ const sourcePaths = [
 const baseDir = path.resolve('docs')
 
 const syncFilesToJson = (sourcePath) => {
-  const noteConfigObject = getMarkdownFiles(sourcePath, baseDir).children
-
-  // console.log(JSON.stringify(noteConfigObject, null, 2))
-
   const targetPath = path.resolve(
     'docs/config',
     `${path.basename(sourcePath)}-sync.json`,
   )
+  const configFilePath = path.resolve(
+    'docs/config',
+    `${path.basename(sourcePath)}.json`,
+  )
+
+  const usedPaths = extractMdPaths(fs.readFileSync(configFilePath, 'utf-8'))
+
+  const noteConfigObject = getMarkdownFiles(
+    sourcePath,
+    baseDir,
+    usedPaths,
+  ).children
+
+  // console.log(JSON.stringify(noteConfigObject, null, 2))
 
   fs.writeFileSync(targetPath, JSON.stringify(noteConfigObject, null, 2))
 }
