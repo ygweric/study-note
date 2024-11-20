93f52022355fa6e1d9576ec44062d61abc48ba35
国wei <ygweric@gmail.com>
2024-11-20 15:00:28 +0800

    1

--- docs/.vuepress/configs/sidebar/index.ts
+++ docs/.vuepress/configs/sidebar/index.ts
@@ -1,12 +1,13 @@
 import type { SidebarOptions } from '@vuepress/theme-default'
-import interview from '../../../config/interview.json'
 import cesium from '../../../config/cesium.json'
 import fe from '../../../config/fe.json'
+import interview from '../../../config/interview.json'
 import tools from '../../../config/tools.json'
 
 export const sidebarZh: SidebarOptions = {
   '/cesium/': cesium,
   '/fe/': fe,
   '/tools/': tools,
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   '/interview/': interview,
 }
